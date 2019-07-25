826e5d64b2a0b7297f865e3a6d35cc2100228dcf
pmahanth967 <pmahanth967@gmail.com>
2019-07-25 19:19:52 +0530

    Updated Project

--- selenium/src/test/java/selenium/ComposeMail.java
+++ selenium/src/test/java/selenium/ComposeMail.java
@@ -14,19 +14,19 @@ import com.objectRepository.ComposeMailObject;
 import com.utilities.ExcelRead;
 
 public class ComposeMail extends LoginAndlogOut{
+	
+	 
 
 	@Test(priority = 3)
 	public void compose() throws Exception
 	{
+		logger=reports.createTest("Compose mail");
+		for(int i=1;i<=3;i++) 
+		{
 		  ComposeMailObject sign1=PageFactory.initElements(driver,ComposeMailObject.class);
-		  
 		  sign1.Compose.click();
 		  Base.waitForElement(sign1.ToSent);
-		  int ColNum=2;
-		  String toSent=ExcelRead.getCellData("Compose", 1, ColNum);
-		  
-		 
-		  System.out.println("No of Counts:"+ColNum);
+		  String toSent=ExcelRead.getCellData("Compose", 1, i);		 
 		  sign1.ToSent.sendKeys(toSent);
 		  String subject=ExcelRead.getCellData("Compose", 3	,1);
 		  sign1.subject.sendKeys(subject);
@@ -34,29 +34,30 @@ public class ComposeMail extends LoginAndlogOut{
 		  sign1.text.sendKeys(body);
 		  sign1.send.click(); 
 		  String message="Sending...";
-		  Base.waitForElement(sign1.Message); String expected=sign1.Message.getText();
+		  Base.waitForElement(sign1.Message); 
+		  String expected=sign1.Message.getText();
 		  Assert.assertEquals(message, expected);	
+		  
+		}
 		 
 	
 	}
 	
-	 @DataProvider(name = "getData")
-	   public Object[][] getData() throws Exception 
-	   { 
-	   String SheetName="Compose"; 
-	   int row=ExcelRead.getRows(SheetName);
-	   int col=ExcelRead.getColumns(SheetName);
-	   System.out.println(row);
-	   System.out.println(col);
-	  Object[][] data=new Object[row][col]; 
-	  for(int rownum=0;rownum<row;rownum++) 
+	
+	  @Test(priority=4)
+	  public void draft() throws Exception
 	  { 
-	  for (int colNum=0;colNum<col;colNum++) 
-	  {
-	  data[rownum][colNum]=ExcelRead.getCellData(SheetName,rownum,colNum);
-	   } 
-	   } 
-	   return data;
+	  ComposeMailObject sign2=PageFactory.initElements(driver,ComposeMailObject.class);
+	  sign2.Compose.click(); 
+	  Base.waitForElement(sign2.ToSent); 
+	  String toSent=ExcelRead.getCellData("Compose", 1, 1); 
+	  sign2.ToSent.sendKeys(toSent);
+	  String subject=ExcelRead.getCellData("Compose", 3 ,1);
+	  sign2.subject.sendKeys(subject); 
+	  String body=ExcelRead.getCellData("Compose", 6, 1); sign2.text.sendKeys(body);
+	  sign2.close.click();
+	  
+	  
 	  }
-
+	 
 }
